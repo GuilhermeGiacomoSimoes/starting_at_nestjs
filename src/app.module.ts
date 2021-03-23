ff720641a725eaa7f963f5fe6b0b311a67179658
Guilherme <trintaeoitogc@gmail.com>
2021-03-22 22:44:35 -0300

    fix: tirado imports de classes desnecessarias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
-import { AdminController } from './admin/admin.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController, AdminController],
-  providers: [AppService, CatsService],
+  controllers: [CatsController],
+  providers: [CatsService],
 })
 export class AppModule {}
