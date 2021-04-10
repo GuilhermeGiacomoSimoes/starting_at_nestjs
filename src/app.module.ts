2b5edec81c0404d3dd2ff09cfbecc047b5cc482c
Guilherme <trintaeoitogc@gmail.com>
2021-04-10 15:52:23 -0300

    feat: fullfiling the request prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,13 @@ import {
 } from '@nestjs/common';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 import { CatsModule } from './cats/cats.module';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [CatsModule],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+    consumer.apply(LoggerMiddleware).forRoutes(CatsController);
   }
 }
